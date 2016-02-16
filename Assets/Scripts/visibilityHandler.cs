60ba71193413d077727e09df1a6d32f5ba359984
Crazydriver8 <krazydriver8@gmail.com>
2016-02-16 00:38:05 -0500

    assets

---
+++ Assets/Scripts/visibilityHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class visibilityHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	IEnumerator Respawn(float delay) {
+			gameObject.SetActive (false);
+			yield return new WaitForSeconds (delay);
+			gameObject.SetActive (true);
+			Debug.Log ("I live again!");
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void visible() {
+		Debug.Log ("I've been spotted!");
+		GameObject cam = GameObject.Find("Main Camera");
+		cam.SendMessage ("triggerRespawn", gameObject);
+	}
+}
