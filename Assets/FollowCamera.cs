700c12c0d8e79eaaca2e7882b7b57bf03c5cf0cc
Crazydriver8 <krazydriver8@gmail.com>
2016-02-16 00:37:12 -0500

    assets folde

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+	public Transform target;
+	public float smoothTime = 0.2f;
+	private Vector3 velocity = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 desiredPosition = target.position;
+		desiredPosition.y = transform.position.y;
+		transform.position = Vector3.SmoothDamp (transform.position, desiredPosition, ref velocity, smoothTime);
+	}
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	GameObject tempObj;
+	float respawnDelay = 2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	IEnumerator Respawn(float delay, GameObject obj) {
+		obj.SetActive (false);
+		yield return new WaitForSeconds (delay);
+		obj.SetActive (true);
+		Debug.Log ("I live again!");
+	}
+
+	void triggerRespawn(GameObject obj) {
+		StartCoroutine (Respawn(respawnDelay, obj));
+	}
+}
